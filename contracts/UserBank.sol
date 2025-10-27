88ad052319a183414ff79447828022e97f852111
Tractorenok <tractorenok63@mail.ru>
2025-10-27 22:22:34 +0400

    5 try

--- contracts/UserBank.sol
+++ contracts/UserBank.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.2 <0.9.0;
-
 
 contract UserBank {
     address public owner;
@@ -12,45 +10,58 @@ contract UserBank {
         uint balance;
         uint[] deposits;
     }
-    mapping(address => bool) public isRegistered;
+
     mapping(address => User) public users;
 
     modifier onlyOwner() {
-        require(msg.sender == owner , "Only owner can call this function");
+        require(msg.sender == owner, "Only owner can call this function");
         _;
     }
-
     constructor() {
         owner = msg.sender;
     }
 
-    function register(string memory _name, uint _age) public  {
-        require(!isRegistered[msg.sender], "User already registered");
-        users[msg.sender] = User(_name, _age, 0, new uint[](0));
+    function register(string memory _name, uint _age) public {
+        require(users[msg.sender].age == 0, "User already registered");
+        users[msg.sender] = User({
+            name: _name,
+            age: _age,
+            balance: 0,
+            deposits: new uint[](0)
+        });
     }
 
-    function deposit(uint amount) public payable {
-        users[msg.sender].balance += msg.value;
-        users[msg.sender].deposits.push(msg.value);
+    function deposit() public payable {
+        User storage user = users[msg.sender];
+        require(user.age > 0, "User not registered");
+
+        user.balance += msg.value;
+        user.deposits.push(msg.value);
     }
 
-    function getBalance () public view returns (uint) {
+    function getBalance() public view returns (uint) {
         return users[msg.sender].balance;
     }
 
-    function getDepositHistory () public view returns (uint[] memory) {
+    function getDepositHistory() public view returns (uint[] memory) {
         return users[msg.sender].deposits;
     }
 
-    function withdraw (uint _amount) public {
-        require(userBalances[msg.sender] >= amount, "Insufficient balance");
-        users[msg.sender].balance -= _amount;
-        payable(msg.sender).transfer(_amount);
+    function withdraw(uint _amount) public {
+        User storage user = users[msg.sender];
+        require(user.balance >= _amount, "Insufficient balance");
+
+        user.balance -= _amount;
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Transfer failed");
     }
 
-    function getUser (address _user) public view returns (string memory, uint, uint) {
-        User storage user = users[_user];
+    function getUser(address _user) public view onlyOwner 
+        returns (string memory name, uint age, uint balance)
+    {
+        User memory user = users[_user];
         return (user.name, user.age, user.balance);
     }
 }
+
 
